871eb84eb89e9ebe4959ad2c0c37e481a1d6256e
lucas-gtd <lucasgoutaudier@gmail.com>
2021-04-15 12:21:16 +0200

    filters form + dynamic input (values storage incoming)

--- src/app/travel-list/travel-list.component.ts
+++ src/app/travel-list/travel-list.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DateRange } from '@angular/material/datepicker';
 @Component({
   selector: 'app-travel-list',
   templateUrl: './travel-list.component.html',
   styleUrls: ['./travel-list.component.css']
 })
 export class TravelListComponent implements OnInit {
+
+  flightRound: boolean = false;
+  simpleFlightDate: DateRange<Date> = new DateRange<Date>(new Date, new Date)
+
+  onSubmit(){
+    console.log(this.simpleFlightDate)
+  }
 
   constructor() { }
 
