1d17ebaa9d6a1627d244cf89e54d20860c632cf1
laura desjacques <laura-desjacques_student2021@wilder.school>
2021-04-15 13:46:29 +0200

    add component 'card-travel' and his child component 'activity'. add  exemple of class in folder 'models' to run one card-travel in the browser

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardTravelComponent } from './card-travel/card-travel.component';
+import { ActivityComponent } from './activity/activity.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardTravelComponent,
+    ActivityComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
