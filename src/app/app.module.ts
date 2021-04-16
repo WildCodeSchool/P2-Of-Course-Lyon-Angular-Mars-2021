4ca21bd359a37994dcb147ec68fe1911c854e94f
laura desjacques <laura-desjacques_student2021@wilder.school>
2021-04-16 10:16:53 +0200

    R.A.S.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { CardTravelComponent } from './card-travel/card-travel.component';
 import { ActivityComponent } from './activity/activity.component';
-import { FormsModule } from '@angular/forms';
-=======
+
 import { HomeComponent } from './home/home.component';
 import { TravelListComponent } from './travel-list/travel-list.component';
 import { NavbarComponent, } from './navbar/navbar.component';
@@ -19,22 +17,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatDatepickerModule, MatDateRangeInput, MatDateRangePicker } from '@angular/material/datepicker'
 import { MatNativeDateModule, MAT_DATE_FORMATS } from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
->>>>>>> 871eb84eb89e9ebe4959ad2c0c37e481a1d6256e
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     CardTravelComponent,
-    ActivityComponent
-=======
+    ActivityComponent,
     HomeComponent,
     TravelListComponent,
     NavbarComponent,
     FooterComponent,
     CategoryCardComponent,
     
->>>>>>> 871eb84eb89e9ebe4959ad2c0c37e481a1d6256e
   ],
   imports: [
     FormsModule,
