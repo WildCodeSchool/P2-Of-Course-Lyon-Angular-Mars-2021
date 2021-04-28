afacd90c3d51c382b16c3abe845199d07e3704d0
Loic699 <loicpimet@gmail.com>
2021-04-28 17:02:31 +0200

    add background to contact

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 import { Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
@@ -8,37 +9,31 @@ import { Validators } from '@angular/forms';
   styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent implements OnInit {
-  //model: Order = new Order();
+
 
  
-  public messages:string=""
+  public mailto:string=""
   constructor(private fb: FormBuilder) {}
   public formulaire: FormGroup;
-  
-
+  public bodyOfMessage: string = "";
   ngOnInit(): void {
-    
+
     this.formulaire = this.fb.group({
      name:['',[Validators.required, Validators.minLength(3), Validators.maxLength(120)]],
      prenom:['',[Validators.required, Validators.minLength(3), Validators.maxLength(120)]],
      email:['',[Validators.required, Validators.email, Validators.minLength(13), Validators.maxLength(120)]],
-     message:['',[Validators.required,Validators.minLength(5), Validators.maxLength(360) ]]
+     message:['',[Validators.required,Validators.minLength(5), Validators.maxLength(360) ]],
+     tableau:this.fb.array([])
     });
-    console.log(this.formulaire)
-    this.messages=`mailto:mohamedboussaid69700@hotmail.fr
-    ?subject=demande d'information&cc=partenaire@hotmail.fr&bcc=avocat@hotmail.fr&body=${this.formulaire.get('message').value}`
-  }
-
-
+    }
 
   onSubmit() {
     // Get form value as JSON object
-    console.log('oderForm submitted : ', this.formulaire.value);
+    this.bodyOfMessage = this.formulaire.get('message').value
+    console.log(this.bodyOfMessage)
+    this.mailto="mailto:mohamedboussaid69700@hotmail.fr?subject=demande d'information&cc=partenaire@hotmail.fr&bcc=avocat@hotmail.fr&body=" + this.bodyOfMessage;
+    console.log(this.formulaire)
+    console.log(this.mailto)
+    console.log(this.bodyOfMessage)
   }
 }
-/*export class Order {
-  email: string;
-  message: string;
-  nom:string;
-  prenom: string;
-}*/
