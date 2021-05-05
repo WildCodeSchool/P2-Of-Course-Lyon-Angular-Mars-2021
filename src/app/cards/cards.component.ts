c276a52da0d4b6ce0150df5a65e4b56516c22c39
lucas-gtd <lucasgoutaudier@gmail.com>
2021-05-05 14:34:16 +0200

    enhanced cart

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,26 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ListVoyages } from '../common/ListVoyages.service';
 import { Voyage } from '../common/Voyage.model';
 
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
-  styleUrls: ['./cards.component.css']
+  styleUrls: ['./cards.component.css'],
 })
 export class CardsComponent implements OnInit {
+  constructor(private service: ListVoyages, private router: Router) {}
 
-  
-  constructor(private service: ListVoyages ) {}
-
-  userCarts : Voyage[];  
-  deleteCarts(carts : Voyage){
-    this.service.deleteCard(carts)
+  userCarts: Voyage[];
+  deleteCarts(carts: Voyage) {
+    this.service.deleteCard(carts);
+  }
+  clickCard(travel: Voyage) {
+    this.service.currentDetailsVoyage = travel;
+    this.router.navigateByUrl('/detailsVoyages');
   }
 
   ngOnInit(): void {
-
-    this.userCarts = this.service.userCart
-
+    this.userCarts = this.service.userCart;
   }
-
 }
